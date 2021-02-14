681f4fe90f1d30b72a770c55d19ce7c7c9ce89e5
IgorDmi <60873886+IgorDmi@users.noreply.github.com>
2021-02-14 21:18:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace factorial
+{
+
+    class Factorial
+    {
+        private int _n;
+        private long _resoult;
+
+        public long factorialCalc(int i)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            return i * factorialCalc(i - 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long resoult = 1;
+            int n = 0;
+
+            Factorial f1 = new Factorial();
+
+            Console.Write("Enter number from will calculate factorial: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(f1.factorialCalc(n));
+
+        }
+    }
+}
